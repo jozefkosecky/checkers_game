d43e080838fc9f76f6c929914078193ff535a53c
Johnny-K1 <jkosecky54@gmail.com>
2023-07-21 21:04:21 +0200

    color white stones

--- board_detection.py
+++ board_detection.py
@@ -45,3 +45,21 @@ def trim_chessboard(image):  # Remove argument here
 
     return cropped_image
 
+
+def color_white_stones(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5,5), 0)
+
+    canny = cv2.Canny(blurred, 200, 1, 1)
+
+    kernel = np.ones((2,2),np.uint8)
+    dilate = cv2.dilate(canny,kernel,iterations = 1)
+
+    contours = cv2.findContours(dilate, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+
+    img_copy = image.copy()
+    for c in contours:
+        cv2.drawContours(img_copy, [c], -1, (50, 50, 50), -1)
+
+    return img_copy
