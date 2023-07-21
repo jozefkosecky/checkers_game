c52c3d532f9714ed4e060a1d06c97adc1bfd4b69
Johnny-K1 <jkosecky54@gmail.com>
2023-07-21 16:29:05 +0200

    trim_chessboard

---
+++ board_detection.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import tkinter as tk
+from PIL import Image, ImageTk
+
+def get_number_of_triming(undistort_image):
+    number_of_triming = 0
+    trimmed_image = None
+    while 1:
+        if(trimmed_image is None):
+            image_for_trim = undistort_image
+        else:
+            image_for_trim = trimmed_image
+        trimmed_image = trim_chessboard(image_for_trim)
+        cv2.imshow("trimming", trimmed_image)
+        key = cv2.waitKey(0)
+        if key == 27:
+            number_of_triming += 1
+            cv2.destroyWindow("trimming")
+            break
+        else:
+            number_of_triming += 1
+
+    return number_of_triming
+            
+def trim_chessboard(image):  # Remove argument here
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+
+    # Threshold the image to separate the black frame
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+    # Find contours in the thresholded image
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Sort the contours by area and keep the largest one
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+    # Get the bounding rectangle for the largest contour
+    x, y, w, h = cv2.boundingRect(contours[0])
+
+    # Crop the image using the bounding rectangle
+    cropped_image = image[y:y+h, x:x+w]
+
+    return cropped_image
+
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+from logging import root
+import tkinter
 import cv2
 from ximea import xiapi
 import numpy as np
 import ximea_camera
+import board_detection
 
 
 def main():
     # create instance of Image to store image data and metadata
     cam = ximea_camera.get_camera()  # call your function with module name prefix
     img = xiapi.Image()
-
-    with np.load('calibration_parameters.npz') as X:
-            mtx, dist = X['mtx'], X['dist']
-
+    
+    number_of_triming = 0
     while 1:
         key = cv2.waitKey(1) & 0xFF
         if key == 27:
@@ -26,7 +27,22 @@ def main():
         cv2.imshow("undistort_image", undistort_image)
 
         if key == ord(' '):
-             cv2.imwrite("image.jpg", undistort_image)
+             cv2.imwrite("image_2.jpg", undistort_image)
+
+    
+        if(number_of_triming == 0):
+            number_of_triming = board_detection.get_number_of_triming(undistort_image)
+
+        trimmed_image = None
+        for trim in range(number_of_triming):
+            if(trimmed_image is None):
+                image_for_trim = undistort_image
+            else:
+                image_for_trim = trimmed_image
+            trimmed_image = board_detection.trim_chessboard(image_for_trim)
+
+        cv2.imshow("trimmed_image", trimmed_image)
+
 
     # stop data acquisition
     print('Stopping acquisition...')
