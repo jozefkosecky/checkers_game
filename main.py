44551cf672f92717ae41c85ae9c3a755c1bbf78c
Johnny-K1 <jkosecky54@gmail.com>
2023-08-13 17:28:28 +0200

    fix info

--- main.py
+++ main.py
@@ -88,7 +88,10 @@ def main():
                 if(was_move_made):
                     who_made_move = game.who_made_move(possible_moves, game_board)
                     if(who_made_move is None):
-                        print_game_board(possible_moves, game_board, player)
+                        if(player == 1):
+                            print_game_board(possible_moves, game_board, 2)
+                        else:
+                            print_game_board(possible_moves, game_board, 1)
                         print("Caka sa na tah od hraca: " + str(player))
                     else:
                         game_board = game.update_checkers_board(possible_moves, game_board)
